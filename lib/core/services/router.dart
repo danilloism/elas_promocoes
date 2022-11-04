f757cce8b8fef2a20cbf3d4a771e57e1ac7f807a
Danillo Ilggner <danilloilggner@gmail.com>
2022-11-04 01:23:45 -0300

    Página de promoção implementada.

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,4 +1,5 @@
 import 'package:elas_promocoes/features/auth/ui/login_page.dart';
+import 'package:elas_promocoes/features/promocoes/ui/promocao_page.dart';
 import 'package:elas_promocoes/main.dart';
 import 'package:fluro/fluro.dart';
 
@@ -7,9 +8,14 @@ class RouterService {
   RouterService(this.router) {
     router.define('/', handler: _homeHandler);
     router.define('/login', handler: _loginHandler);
+    router.define('/:id', handler: _promocaoPageHandler);
   }
   Handler get _homeHandler =>
       Handler(handlerFunc: (_, __) => const MyHomePage());
   Handler get _loginHandler =>
       Handler(handlerFunc: (_, __) => const LoginPage());
+  Handler get _promocaoPageHandler => Handler(handlerFunc: (context, params) {
+        final id = params['id']![0];
+        return PromocaoPage(promocaoId: id);
+      });
 }
